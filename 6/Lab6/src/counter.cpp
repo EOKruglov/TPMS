8708e6bf5e6f704e8f0c11ffe2e08ec1efdfd456
EOKruglov <ye.kruglov@yandex.ru>
2019-12-09 22:17:46 +0300

    6

---
+++ 6/Lab6/src/counter.cpp
@@ -0,0 +1,41 @@
+#include "counter.h"
+#include <QDebug>
+
+Counter::Counter() : QObject()
+{
+    this->last = 1;
+    this->prelast = 1;
+    this->count = 0;
+}
+
+void Counter::increment()
+{
+    //this->prelast = this->last;
+    //this->last = this->count;
+    this->count = this->count + 1;
+    emit countChanged();
+}
+
+void Counter::reset()
+{
+    this->count = 0;
+    this->prelast = 1;
+    this->last = 1;
+    emit countChanged();
+}
+
+void Counter::print()
+{
+    qDebug() << "Current count: " << this->count;
+}
+
+int Counter::getCount()
+{
+    return this->count;
+}
+
+void Counter::setCount(int count)
+{
+    this->count = count;
+    emit countChanged();
+}
---
+++ 6/Lab6/src/liststring.cpp
@@ -0,0 +1,26 @@
+#include "liststring.h"
+
+ListStrings::ListStrings() : QObject()
+{
+    this->list = QStringList();
+}
+
+void ListStrings::append(QString str)
+{
+    this->list.append(str.toLower());
+    emit listChanged();
+}
+
+void ListStrings::removeLast()
+{
+    if (this->list.count()) {
+        this->list.removeLast();
+    }
+    emit listChanged();
+}
+
+QString ListStrings::getText()
+{
+    QString text = this->list.join(", ");
+    return text.replace(0, 1, text[0].toUpper());
+}
